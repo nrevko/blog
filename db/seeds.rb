1e01c92dfbd0c5f008fb47f4037ca60ed64c5f0b
Nina R <nina.revko@gmail.com>
2013-11-27 07:04:13 -0800

    Adding faker gem with generation of fake Post/Comment/Tag

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# generate 100 posts with 5  to 10 comments each and 100 tags
+
+100.times do
+  post = Post.create(:body=>Faker::Lorem.paragraphs.join("\n"))
+  
+  rand(5..10).times do
+    Comment.create(:post_id=>post.id, :body=> Faker::Lorem.sentence)
+  end
+end
+
+100.times do
+  Tag.create(:name => Faker::Lorem.word)
+end
